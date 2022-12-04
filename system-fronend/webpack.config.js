3908a7be51d935e2288a72600fe6a364a6b3ddc4
Tptrix29 <1953776@tongji.edu.cn>
2022-12-04 14:39:19 +0800

    config

--- system-fronend/webpack.config.js
+++ system-fronend/webpack.config.js
@@ -4,11 +4,13 @@ const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        index: './src/index.js'
+        index: './src/index.js',
+        login: './src/user-management-module/pages/login.js'
     },
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build')
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true
     },
     module: {
         rules: [{
@@ -27,19 +29,33 @@ module.exports = {
             use:{
                 loader: 'css-loader',
             }
+          },
+          {
+            test: /\.(jpg|jpe?g|gif)$/,
+            exclude: /node_modules/,
+            use:{
+                loader: 'file-loader',
+            }
           }
         ]
     },
     devServer: {
         open: true,
         hot: true,
-        port:3001,
+        port: 3001,
         static: "./public"
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new HtmlPlugin({
-            template: 'public/index.html'
-        })
+            template: 'public/index.html',  // 模版，不需改动
+            filename: 'index.html',  // 输出文件名称，浏览器访问路径，HTML文件名默认index.html，需要改动
+            chunks: ['index'],  // js入口，对应entry中的key，需要改动
+        }),
+        new HtmlPlugin({
+            template: 'public/index.html',
+            filename: 'login/index.html',
+            chunks: ['login'],
+        }),
     ]
 }
