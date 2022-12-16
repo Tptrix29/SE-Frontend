5ae97abee16a51c6731f3a21714952f4350b6585
Tptrix29 <1953776@tongji.edu.cn>
2022-12-16 14:22:05 +0800

    change webpack config

--- system-fronend/webpack.dev.js
+++ system-fronend/webpack.dev.js
@@ -1,6 +1,5 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
+const {merge} = require('webpack-merge');
+const common = require('./webpack.common')
 
 module.exports = merge(common, {
     devServer: {
