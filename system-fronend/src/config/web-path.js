b5bef7c220110c5f4de5f69b040fe83223774acc
Tptrix29 <1953776@tongji.edu.cn>
2022-12-23 00:10:19 +0800

    host config

--- system-fronend/src/config/web-path.js
+++ system-fronend/src/config/web-path.js
@@ -1,8 +1,10 @@
 
 
 export class WebPathConfig{
-    static host = 'http://localhost:3001';
+    // static host = 'http://localhost:3001';
     // static host = 'http://120.78.65.145:80';
+    static host = "http://" + window.location.host;
+
     static toURL(path, params=null){
         if(!params)
             window.location = this.host + path;
